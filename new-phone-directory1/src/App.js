87b1a813c41785efd57e79f8a6c59fd6b154d1eb
Xfreak22 <139982849+Xfreak22@users.noreply.github.com>
2024-02-17 11:45:58 +0530

    Created Skeleton of Home Page
    Created Skeleton of Home Page

---
+++ new-phone-directory1/src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div>
+    {/* <div className="Header">
+      Phone directory
+    </div>
+    <button>Add</button>
+    <div>
+     <span>Name</span><br/>
+     <span>Phone</span>
+    </div> */}
+    <label htmlfor="name">Name: </label>
+    <input id="name" text="text" placeholder="Type here" defaultValue="Suraj"/>
+    </div>
+  );
+}
+
+export default App;
