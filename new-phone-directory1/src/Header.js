797a4a3fa86f90d8117825179dbdd97a4d835b08
Xfreak22 <139982849+Xfreak22@users.noreply.github.com>
2024-02-28 22:52:02 -0800

    Styled ‘Header’ (External Styling)
    Styled ‘Header’ (External Styling)

--- new-phone-directory1/src/Header.js
+++ new-phone-directory1/src/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const Header = function() {
+    const HeaderStyle = {background:"black" , padding: 20, textTransform: "uppercase", color: "white" , textAlign: "center" };
     return(
-        <div className="Header">
+        <div style={HeaderStyle}>
             Phone Directory
         </div>
     )
