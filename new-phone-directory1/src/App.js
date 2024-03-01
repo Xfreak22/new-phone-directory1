0c760b3bebd10dc5ba7fd2a215e7bd2c75095ea1
Xfreak22 <139982849+Xfreak22@users.noreply.github.com>
2024-03-01 06:05:02 -0800

    Rendered Subscribers Dynamically
    Rendered Subscribers Dynamically

--- new-phone-directory1/src/App.js
+++ new-phone-directory1/src/App.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react';
 import Header from './Header';
-
+import Button1 from './button';
 
 class App extends Component {
   render() {
+    let subscriberlist = [
+      {
+        id: 1,
+        Name: "Suraj",
+        Phone: 9319236003
+      },
+      {
+        id: 2,
+        Name: "Manish",
+        Phone: 8144813417
+      }
+    ]
     return (
       <div>
       <Header/>  
     <div>
-    <button>Add</button>
+      <Button1/>
     <div/>
     <div>
-     <span>Name</span><br/>
-     <span>Phone</span>
+     <span style={{color: "darkslategrey", margin: "auto", padding: 10, width: "auto"}}>Name</span>
+     <span style={{color: "darkslategrey", margin: "auto", padding: 10, width: "auto"}}>Phone</span>
     </div>
+ 
+      {
+        subscriberlist.map(sub =>{
+         return <div key={sub.id}>
+                <span style={{color: "darkslategrey", margin: "auto", padding: 10, width: "auto"}}>{sub.Name}</span>
+                <span style={{color: "darkslategrey", margin: "auto", padding: 10, width: "auto"}}>{sub.Phone}</span>       
+         </div>
+        })
+      } 
+
     </div>
     </div>
     )
---
+++ new-phone-directory1/src/button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button1 = function() {
+    const buttonstyle = {background: "lightgreen", color: "white", margin: 10, padding: 10, width: 70, border: "none"} 
+    return(
+    <div>
+        <button style={buttonstyle}>Add</button>
+    </div>
+    )
+}
+
+export default Button1;
